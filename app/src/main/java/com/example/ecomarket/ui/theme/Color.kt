468b3e993636149c534b0277239aa775a06e7b97
Hapema <Haroldmartes62@gmail.com>
2025-10-27 00:16:50 -0300

    Se agrega nueva palera de colores para la pasteleria

--- app/src/main/java/com/example/ecomarket/ui/theme/Color.kt
+++ app/src/main/java/com/example/ecomarket/ui/theme/Color.kt
@@ -3,9 +3,14 @@ package com.example.ecomarket.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Crema = Color(0xFFF7EEE6)
+val Rosado = Color(0xFFF9D7DD)
+val Cafe = Color(0xFF7A3E10)
+val CafeOscuro = Color(0xFF5B2D0B)
+val GrisTexto = Color(0xFF4A4A4A)
+val ChipPink = Color(0xFFFFE6EE)
+val SurfaceVariant = Color(0xFFFFF6F0) // bordes/superficies suaves
+val OutlineSoft    = Color(0xFFDED3C8) // líneas/dividers
+val GrisClaroTexto = Color(0xFFEDE5DE) // texto en dark
+val FondoDark      = Color(0xFF1E1B18) // fondo oscuro cálido tipo cacao
